09cd4eaf9c483410f7d067c6cb65a939707de2a9
FlaAssis <flah.assis@gmail.com>
2022-11-16 13:35:46 -0300

    corrigindo identação

--- coord.py
+++ coord.py
@@ -14,33 +14,32 @@ from mpl_toolkits.mplot3d import Axes3D
 import sys
 
 if (len(sys.argv) !=4):
- print("Invalid number of arguments.... try run \"python exec.py <input.csv> <boxkant(nm)> <output>\"")
+    print("Invalid number of arguments.... try run \"python exec.py <input.csv> <boxkant(nm)> <output>\"")
 else:
-# input_file = ""
- try:
-  input = open(sys.argv[1],'r')
-  box = sys.argv[2]
-  output = sys.argv[3]
-  dataset = pd.read_csv(input, sep=';',parse_dates=True)
- except IOError:
-  print("File not found...")
-  exit()
- print('Running...')
- type(dataset)
- dataset.head()
- dataset.columns
- dataset.count()
- dataset.describe()
- threedee = plt.figure().gca(projection='3d')
- threedee.scatter(dataset['x1'], dataset['y1'], dataset['z1'], s=10)
- threedee.set_xlabel('x1')
- threedee.set_ylabel('y1')
- threedee.set_zlabel('z1')
- plt.ylim([0,float(box)])
- plt.xlim([0,float(box)])
- threedee.set_zlim(0,float(box))
- plt.savefig(str(output)+'.png')
- plt.show()
+    try:
+         input = open(sys.argv[1],'r')
+         box = sys.argv[2]
+         output = sys.argv[3]
+         dataset = pd.read_csv(input, sep=';',parse_dates=True)
+    except IOError:
+        print("File not found...")
+        exit()
+    print('Running...')
+    type(dataset)
+    dataset.head()
+    dataset.columns
+    dataset.count()
+    dataset.describe()
+    threedee = plt.figure().gca(projection='3d')
+    threedee.scatter(dataset['x1'], dataset['y1'], dataset['z1'], s=10)
+    threedee.set_xlabel('x1')
+    threedee.set_ylabel('y1')
+    threedee.set_zlabel('z1')
+    plt.ylim([0,float(box)])
+    plt.xlim([0,float(box)])
+    threedee.set_zlim(0,float(box))
+    plt.show()
+    plt.savefig(str(output)+'.png')
 
  print('**END**')
 
